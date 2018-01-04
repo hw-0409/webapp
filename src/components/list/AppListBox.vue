c559ca79ebdad80f7d5bb5def8a24d300677cdc4
nice3 <1032878312@qq.com>
2018-01-04 21:45:57 +0800

    列表页布好局

---
+++ src/components/list/AppListBox.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="app-list-box">
+    	<ul class="top">
+    		<li>全部分类<i class="yo-ico">&#xf031;</i></li>
+    		<li>综合分类<i class="yo-ico">&#xf031;</i></li>
+    	</ul>
+    	
+    	<AppListItem></AppListItem>
+    	
+    </div>
+</template>
+
+<script>
+	import AppListItem from './AppListItem'
+	
+	export default {
+        name:'app-list-box',
+        components:{AppListItem},
+        
+        data(){
+        	return {
+        		
+        	}
+        }
+    }
+	
+</script>
+
+<style lang="scss" scoped>
+	
+	.app-list-box{
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		margin-left: 0.1rem;
+		.top{
+			display: flex;
+			height:0.48rem;
+			border-bottom: 1px solid #ccc;
+			line-height: 0.48rem;
+		    text-align: center;
+		    li{
+		    	width: 50%;
+		    	font-size: 14px;
+		    }
+		}
+		
+	}
+</style>
