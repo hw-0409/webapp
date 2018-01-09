41fb30c156adb552a9b537bfb96d5b8e89f86cb7
hw-0409 <15835850014@163.com>
2018-01-09 17:43:31 +0800

    vuex拆分

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,4 +1,4 @@
-import Vue from "vue"
+﻿import Vue from "vue"
 import Vuex from "vuex"
 Vue.use(Vuex)
 
@@ -10,34 +10,6 @@ const state = {
     address:''
 }
 
-const mutations = {
-    
-    initInfo(state, info) {
-        state.info = info
-    },
-    //定位，获取到定位信息后，加载其他的内容
-    getPosition(state,info){
-        state.address = info.address
-    }
-	
-        
-}
-const actions = {
-    initInfo({ commit }) {
-        setTimeout(() => {
-            let results = localStorage.axfInfo || ''
-            commit('initInfo', results)
-        }, 300);
-    },
-    getPosition({ commit }){
-          setTimeout(() => {
-           // console.log(getPosition)
-            	getPosition((info)=>{
-			    commit('getPosition',info)
-                 })
-         }, 2000);
-    }
-}
 
 const store = new Vuex.Store({
     state,
